7029c07be86df3b91ab4de9395053e688e292e7a
jcamsfo <jc@jimcampbell.tv>
2024-08-03 20:30:46 -0700

    2nd commit

---
+++ timing.h
@@ -0,0 +1,11 @@
+#ifndef TIMING_H
+#define TIMING_H
+
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <string>
+
+// Load a grayscale image
+void Loop_Timing(bool show_timing);
+
+#endif // TIMING_H
